3fa61b5615d430b9c91b297627ca6dc50da3fcce
Pablo Vallejo <p.vallejo@astroline.es>
2020-12-15 16:39:01 +0100

    Primer commit

---
+++ models/sockets.js
@@ -0,0 +1,29 @@
+
+class Sockets {
+ 
+    constructor ( io ) {
+
+        this.io = io;
+
+        this.socketEvents();
+
+    }
+
+    socketEvents() {
+
+        // ON connection
+        this.io.on('connection', (socket) => { 
+        
+            // Escuchar evento: mensaje-to-server
+            socket.on('mensaje-to-server', (data) => {
+                console.log(data); 
+                this.io.emit('mensaje-from-server', data );
+            });
+            
+         });
+    }
+
+
+}
+
+module.exports = Sockets;
